433d3693161bf55e01a44d1821b35511f977146c
Prakrut Chaubal <prakrutchaubal@gmail.com>
2023-10-25 15:16:21 +1100

    first commit on testing

---
+++ tests/test.py
@@ -0,0 +1,18 @@
+import unittest
+
+class Test(unittest.TestCase):
+
+    def test_tsz(self):
+        pars = {
+                'H0': 70,
+                'omh2': 0.3,
+                'ombh2': 0.05,
+                'sigma8': 0.8,
+                'ns': 0.96,
+                'As': 2,
+                'tau': 0.056,
+                
+        }
+
+if __name__ == '__main__':
+    unittest.main()
